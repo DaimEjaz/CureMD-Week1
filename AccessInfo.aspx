3721d18e0e3d46e93e9bc21489fc378d65d7f816
Daim Ejaz <72712118+DaimEjaz@users.noreply.github.com>
2022-07-20 16:04:40 -0400

    Add files via upload

---
+++ AccessInfo.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AccessInfo.aspx.cs" Inherits="AccessStudentInfo.AccessInfo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Select any name to display the complete information!</h1>
+            <asp:DropDownList runat="server" ID="list"></asp:DropDownList>
+            <p id="result"></p>
+        </div>
+    </form>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        const dropdown = document.getElementById("list");
+
+        dropdown.addEventListener("change", () => {
+            let queryIndex = $("#list option:selected").index()
+            $.ajax({
+                url: 'AccessInfo.aspx',
+                type: 'post',
+                data: JSON.stringify({ "queryIndex": queryIndex }),
+                contentType: 'application/json',
+                async: false,
+                success: function (data) {
+                    if (data.d) {
+                        $("#result").text = data.d;
+
+                    }
+                    else {
+                        $("#result").text("Error...");
+
+                        throw new DOMException("Invalid value supplied");
+
+                    }
+                }
+            });
+        })
+    </script>
+</body>
+</html>
---
+++ AccessInfo.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccessStudentInfo
+{
+    public partial class AccessInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            List<Student> studentList = new List<Student>();
+            string[] nameArr = { "Edgar Barber", "Marquis Reilly", "Isabell Frank", "Issac Hammond", "Aydan Mcclain", "Daphne Burke", "Mia Duarte", "Phoebe Mcclain", "Dayana Massey", "Maxwell Zhang", "Keith Pineda", "Dustin Camacho", "Alyvia Lam", "Justice Mata", "Alissa Bautista", "Laura Wade", "Howard Flynn", "Marques Wilcox", "Taryn Wang", "Caden Potts" };
+            StudentGenerator(nameArr, studentList);
+            //Console.WriteLine(studentList);
+            list.DataSource = nameArr;
+            list.DataBind();
+        }
+
+        public static void StudentGenerator(string[] nameArr, List<Student> studentList)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < nameArr.Length ; i++)
+            {
+                studentList.Add(new Student(nameArr[i], i+1, rnd.Next(2017, 2022) ));
+            }
+        }
+
+        public static object Answer(List<Student> studentList )
+        {
+            //int queryIndex = 
+            return studentList[queryIndex];
+        }
+
+
+
+    }
+
+    public class Student 
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int AdmissionYear { get; set; }   
+
+        public Student(string name, int id, int admissionYear)
+        {
+            Name = name;
+            Id = id;
+            AdmissionYear = admissionYear;
+        }
+    }
+
+    
+
+}
