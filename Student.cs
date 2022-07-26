e925d04cc772f14eb334af9e5c411178fb0629ac
Daim Ejaz <72712118+DaimEjaz@users.noreply.github.com>
2022-07-26 06:02:27 -0400

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentGrid
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Address { get; set; }
+
+        public List<Student> studentList = new List<Student>();
+
+        public Student()
+        {
+            //nothing here
+        }
+        public Student(string studentName, int studentId, string studentAddress)
+        {
+            Name = studentName;
+            RollNo = studentId;
+            Address = studentAddress;
+        }
+        public List<Student> StudentGenerator(string[] nameArr, string [] addressArr)
+        {
+            //Random rnd = new Random();
+
+            for (int i = 0; i < nameArr.Length; i++)
+            {
+                studentList.Add(new Student(nameArr[i], i + 1, addressArr[i]));
+            }
+
+            return studentList;
+        }
+    }
+
+   
+}
---
+++ StudentGrid.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentGrid
+{
+    public partial class Studentgrid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] nameArr = { "Edgar Barber", "Marquis Reilly", "Isabell Frank", "Issac Hammond", "Aydan Mcclain", "Daphne Burke", "Mia Duarte", "Phoebe Mcclain", "Dayana Massey", "Maxwell Zhang" };
+            string[] addressArr = { "753 Schinner Lake", "15504 O'Kon Plaza", "10281 Moore Island", "58552 Fermin Harbors", "786 Schmitt Crescent", "4028 Swaniawski Pass", "12616 Allison Inlet", "19745 Emily Street", "5733 Winifred Motorway", "678 McClure Course"};
+            List<Student> studentList = new Student().StudentGenerator(nameArr, addressArr);
+        }
+    }
+
+    
+}
